c68140bd6bdbdfc7cc8a3c20ef2fe4bbe97db33a
Ravi Leal <pauloravileal@outlook.com>
2021-04-20 11:58:49 -0300

    dia2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express = require('express')
+import "./database"
+import {routes} from "./routes"
 
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.get("/", (request: express.Request, response: express.Response) => {
   return response.json({
